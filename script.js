be0aacbd07642564049a9bb2686d72cbd8a4f2b8
Hershell Koger <hersheykoger@yahoo.com>
2022-10-21 07:53:20 -0400

    Create Book object constructor that takes title, author, pages and read status; attach .logInfo method to Book.prototype

---
+++ script.js
@@ -0,0 +1,15 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.logInfo = function () {
+    if(this.read) {
+        return `${this.title} by ${this.author}, ${this.pages} pages, book read`
+    } else {
+        return `${this.title} by ${this.author}, ${this.pages} pages, not read`
+    }
+}
+
